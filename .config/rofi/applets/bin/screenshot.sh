fbf151a26b3802dbd4f4c1c0ecc3194ee6751290
JoshuaMarkle <joshuamarkle25@gmail.com>
2023-10-01 00:20:13 -0400

    Added emacs and update vim

--- .config/rofi/applets/bin/screenshot.sh
+++ .config/rofi/applets/bin/screenshot.sh
@@ -11,7 +11,7 @@ theme="$type/$style"
 
 # Theme Elements
 prompt='Screenshot'
-mesg="DIR: `xdg-user-dir PICTURES`/Screenshots"
+mesg="DIR: pictures/screenshots"
 
 if [[ "$theme" == *'type-1'* ]]; then
 	list_col='1'
@@ -67,7 +67,7 @@ run_rofi() {
 # Screenshot
 time=`date +%Y-%m-%d-%H-%M-%S`
 geometry=`xrandr | grep 'current' | head -n1 | cut -d',' -f2 | tr -d '[:blank:],current'`
-dir="`xdg-user-dir PICTURES`/Screenshots"
+dir="pictures/screenshots"
 file="Screenshot_${time}_${geometry}.png"
 
 if [[ ! -d "$dir" ]]; then
